73917038ef7824559e4d0a1c6025e29955bbabe1
Mao2414 <maureen.ramirez1@ulatina.net>
2025-04-01 22:26:26 -0600

    Creacion de metodos Firebase para el documento Notas

--- app/src/main/java/com/Notes/proyecto_carlos_maureen/myapplication/data/database/Firebase/Notas_Firebase.kt
+++ app/src/main/java/com/Notes/proyecto_carlos_maureen/myapplication/data/database/Firebase/Notas_Firebase.kt
@@ -1,2 +1,95 @@
 package com.Notes.proyecto_carlos_maureen.myapplication.data.database.Firebase
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.Notes.proyecto_carlos_maureen.myapplication.model.NotasFB
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import javax.inject.Inject
+
+class Notas_Firebase @Inject constructor(private val auth: FirebaseAuth,
+                                         private val firestore: FirebaseFirestore) {
+
+    private val coleccion1="AplicationBD"
+    private val notasColeccion="Notas"
+
+    private val Usuario:String
+        get() = auth.currentUser?.email?: throw IllegalStateException("Usuario no autenticado")
+
+
+
+
+
+    fun guardarNotas(nota: NotasFB){
+        val document = if (nota.id.isEmpty()) {
+            // Nuevo documento con ID automático
+            firestore.collection(coleccion1)
+                .document(Usuario)
+                .collection(notasColeccion)
+                .document()
+                .also { nota.id = it.id }
+        }else{
+            // Si encuentra el id de la nota, la actualiza
+            firestore
+                .collection(coleccion1)
+                .document(Usuario)
+                .collection(notasColeccion)
+                .document(nota.id)
+
+
+        }
+        document.set(nota)
+            .addOnFailureListener { e ->
+                Log.e("Firestore", "Error al guardar documento", e)
+            }
+
+
+    }
+
+    fun eliminarNotas(nota:NotasFB) {
+
+        if (nota.id.isNotEmpty()) {
+            firestore
+                .collection(coleccion1)
+                .document(Usuario)
+                .collection(notasColeccion)
+                .document(nota.id)
+                .delete()
+                .addOnSuccessListener {
+                    Log.d("DeleteObjeto", "Objeto eliminado")
+                }
+                .addOnCanceledListener {
+                    Log.e("DeleteObjeto", "Objeto NO eliminado")
+
+                }
+
+        }
+    }
+    fun getNotas():MutableLiveData<List<NotasFB>> {
+        val listaNotas = MutableLiveData<List<NotasFB>>()
+        firestore
+            .collection(coleccion1)
+            .document(Usuario)
+            .collection(notasColeccion)
+            .addSnapshotListener { instantanea, _error ->
+
+                if (_error != null) {
+                    return@addSnapshotListener
+                }
+                if (instantanea != null) {
+
+                    val lista = ArrayList<NotasFB>()
+                    instantanea.documents.forEach {
+                        var objetos = it.toObject(NotasFB::class.java)
+                        if (objetos != null) {
+                            lista.add(objetos)
+                        }
+                    }
+
+
+                    listaNotas.value = lista
+                }
+            }
+        return listaNotas
+    }
+}
